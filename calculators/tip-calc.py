7c33ab86e89b9337837d79927dc979d65439cd65
luka.danger <lukathevegan@gmail.com>
2023-01-11 17:02:45 -0700

    Initial Commit
    This program does a basic function. It asks you to input the total amount of the bill, the percentage of tip you would like to give, and how many people are splitting the bill. Once these three variables have values, the program calculates your tip and total amount each person should pay.
    
    Later updates of this program will prevent the program from breaking when a value other than an integer or float are added to the program.

---
+++ calculators/tip-calc.py
@@ -0,0 +1,9 @@
+print("Welcome to the tip calculator! Don't forget to tip your server! ")
+
+bill = float(input("What was the total bill?: ")) 
+tip = float(input("What percentage tip would you like to give? 18, 20, or 22? "))
+split = float(input("How many people to split the bill? "))
+percent = (0.01 * float(tip)) + 1 
+total = round((bill * percent) / split , 2) 
+
+print(f'Each person should tip ${tip}, for a total of ${total}')
