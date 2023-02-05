e4e2c27940c1e22446303f1f56c761de35066020
luka.danger <lukathevegan@gmail.com>
2023-02-04 17:43:18 -0700

    Add variable

--- calculators/tip-calc.py
+++ calculators/tip-calc.py
@@ -5,5 +5,6 @@ tip = float(input("What percentage tip would you like to give? 18, 20, or 22? ")
 split = float(input("How many people to split the bill? "))
 percent = (0.01 * float(tip)) + 1 
 total = round((bill * percent) / split , 2) 
+total_tip = (total - bill)
 
-print(f'Each person should tip ${tip}, for a total of ${total}')
+print(f'Each person should tip ${total_tip}, for a total of ${total}')
