6eba6645afccb1b95e4669fab9e9bb767f7f6f60
luka.danger <lukathevegan@gmail.com>
2023-03-01 09:42:31 -0700

    Initial Commit
    This Python Project uses a list and a for loop to find the average heights (inputted in cm).
    
    Other thoughts...is inputted really a word?

---
+++ calculators/height.py
@@ -0,0 +1,28 @@
+#Pseudocode:  
+#When a user inputs heights (in cm)
+#Count the number of inputted heights 
+#Add heights together
+#Divide the sum of heights by the total number of inputs
+#Print the average height 
+
+#Instructions: Make sure to read documentation top down! 
+
+#To input user heights (Line 2)
+student_heights = input("Input a list of student heights\n").split()
+#These line place the inputted heights into a list
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n]) 
+
+#Sets variables to 0
+heights_in_list = 0 
+total = 0
+#Use for loop to find the number of heights in the list (Line 3)
+for i in student_heights:
+    heights_in_list+=1 
+#Finds the sum of all heights added together (Line 4)
+count = sum(student_heights)
+#Divides the sum of all heights by the number of heights in list (Line 5)
+average_height = count / heights_in_list
+#Prints the average height (Line 6)
+#Use round() to get an integer 
+print(round(average_height)) 
